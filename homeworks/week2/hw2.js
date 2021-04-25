f1652173205d4e863cb730fe0722ba0ee065ce6c
Jess <rj603295@gmail.com>
2021-04-25 15:58:12 +0800

    commit week2-HW

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,10 @@
 function capitalize(str) {
-  
+  let firstS = str[0].toUpperCase()
+  let ans = firstS
+  for(let i=1; i<str.length; i++){
+    ans += str[i]
+  }
+  return ans
 }
 
 console.log(capitalize('hello'));
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,9 +1,18 @@
 function join(arr, concatStr) {
-  
+  let str = ""
+  for (let i=0; i<arr.length-1; i++) {
+    str += arr[i] + concatStr
+  }
+  str += arr[arr.length-1]
+  return str
 }
 
 function repeat(str, times) {
-  
+  let ans = ""
+  for (let i=1; i<=times; i++){
+    ans += str
+  }
+  return ans
 }
 
 console.log(join(['a'], '!'));
